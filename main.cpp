a889e3d1e64dcefdf337eea1d23c27715fab638d
SouthKartman <hfhdvdffvv@gmail.com>
2026-03-29 17:16:56 +0300

    fix main

--- main.cpp
+++ main.cpp
@@ -1,20 +1,28 @@
-#ifdef Q_OS_ANDROID
-#include <QtCore/qnativeinterface.h>
-#include <QCoreApplication>
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
-#include "audiolevel.h" // Подключаем наш новый файл
+#include "audiolevel.h"
+
+// Если нужны специфичные инклуды для Android, пишем их тут
+#ifdef Q_OS_ANDROID
+#include <QtCore/qnativeinterface.h>
+#endif
 
 int main(int argc, char *argv[]) {
     QGuiApplication app(argc, argv);
     
-    // Регистрируем тип
+    // Этот код общий для всех платформ
     qmlRegisterType<AudioLevel>("CyberMedia", 1, 0, "AudioLevel");
 
     QQmlApplicationEngine engine;
     const QUrl url(QStringLiteral("qrc:/kursach/Main.qml"));
+    
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+        &app, [url](QObject *obj, const QUrl &objUrl) {
+            if (!obj && url == objUrl)
+                QCoreApplication::exit(-1);
+        }, Qt::QueuedConnection);
+
     engine.load(url);
     
     return app.exec();
 }
-#endif
